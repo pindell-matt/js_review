969434f2936aaaa16cf74a3e24f349a9c1b224be
Matt Pindell <pindell.m@gmail.com>
2016-09-09 20:54:23 -0400

    Create test.js file

---
+++ es6/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const sentences = [
+  { subject: 'JavaScript', verb: 'is', object: 'great' },
+  { subject: 'Elephants', verb: 'are', object: 'large' },
+];
+
+// es6 feature: object destructuring
+function say({ subject, verb, object }) {
+  // es6 feature: template strings
+  console.log(`${subject} ${verb} ${object}`);
+}
+
+// es6 feature: for..of
+for(let s of sentences) {
+  say(s);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 gulp.task('default', function() {
-  // gulp tasks: tbd
+  // Node source
+  gulp.src("es6/**/*.js")
+      .pipe(babel())
+      .pipe(gulp.dest("dist"));
+  // browser source
+  gulp.src("public/es6/**/*.js")
+      .pipe(babel())
+      .pipe(gulp.dest("public/dist"));
 });
